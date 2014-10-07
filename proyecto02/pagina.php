0c1b750ae93c0f2273122a82a7ed03595eb2e63d
Paco Gomez <fjgomez@florida-uni.es>
2014-10-07 12:20:43 +0200

    Anyadimos tarea16

---
+++ proyecto02/pagina.php
@@ -0,0 +1,28 @@
+<?php
+require_once "cabecera.php";
+require_once "cuerpo.php";
+require_once "pie.php";
+
+class pagina
+{
+	public $titulo="TITULO DE LA PAGINA";
+	private $cabecera,$cuerpo,$pie;
+	
+	function __construct($numFilas,$numColumnas)
+	{
+		$this->cabecera = new cabecera();
+		$this->cabecera->setMenuGoogle(5);
+		$this->cuerpo = new cuerpo;
+		$this->cuerpo->setTabla($numFilas,$numColumnas);
+		$this->pie = new pie;
+		$this->pie->setPie();
+	}
+	
+	function getPagina()
+	{
+		echo $this->cabecera.$this->cuerpo.$this->pie;
+	}
+	
+	
+}
+?>
